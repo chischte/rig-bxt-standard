dfd5618fcaf7ca5d8e856b9fa8e562756fcbc436
Michael Wettstein <chischte@gmx.ch>
2019-10-19 13:06:55 +0200

    Add interrupt function and error log, code not working yet

--- BXT_STANDARD_RIG/BXT_STANDARD_RIG.ino
+++ BXT_STANDARD_RIG/BXT_STANDARD_RIG.ino
@@ -7,11 +7,56 @@
  * Michael Wettstein
  * October 2019, Zürich
  * *****************************************************************************
+ * TODO:
+ * SYSTEM RESET FUNKTION HINZUFÜGEN:
+ * Add interrupt for start/stop button.
+ * // attachInterrupt(digitalPinToInterrupt(pin), ISR, mode)
+   //
+   //const byte digital_output = CONTROLLINO_D0;
+   //const byte interruptPin = CONTROLLINO_IN1;
+   //volatile byte state = LOW;
+   //
+   //void setup() {
+   // pinMode(digital_output, OUTPUT);
+   // pinMode(interruptPin, INPUT);
+   // attachInterrupt(digitalPinToInterrupt(interruptPin), blink, CHANGE);
+   //}
+   //
+   //void loop() {
+   // digitalWrite(digital_output, state);
+   //}
+   //
+   //void blink() {
+   // state = !state;
+   //}
+    *
+    * ALSO DEBOUNCE THE INTERRUPT BUTTON:
+//    * void my_interrupt_handler()
+//{
+// static unsigned long last_interrupt_time = 0;
+// unsigned long interrupt_time = millis();
+// // If interrupts come faster than 200ms, assume it's a bounce and ignore
+// if (interrupt_time - last_interrupt_time > 200)
+// {
+//   ... do your thing
+// last_interrupt_time = interrupt_time;
+// }
+//}
+    *
+ * reset soll bei jedem neustart durchgeführt werden
+ * TIMEOUTFUNKTION HINZUFÜGEN
+ * nach Ablauf der Timeoutzeit von 40" soll ein reset durchgeführt werden
+ * es soll auf dem eeprom gespeichert werden in welchem schritt der reset wie oft durchgeführt wurde
+ * nach dem 3. timeout soll das rig ausschalten und ein error blink soll laufen
+ * wenn die machine nicht läuft soll alle 3 sekunden ausgegeben werden bei welchem Schritt wie oft ein
+ * reset durchgeführt wurde.
  */
 
-#include <Controllino.h> // https://github.com/CONTROLLINO-PLC/CONTROLLINO_Library
-#include <Cylinder.h>    // https://github.com/chischte/cylinder-library
-#include <Debounce.h>    // https://github.com/chischte/debounce-library
+#include <Controllino.h>    // https://github.com/CONTROLLINO-PLC/CONTROLLINO_Library
+#include <Cylinder.h>       // https://github.com/chischte/cylinder-library
+#include <Debounce.h>       // https://github.com/chischte/debounce-library
+#include <EEPROM_Counter.h> // https://github.com/chischte/eeprom-counter-library
+#include <Insomnia.h>       // https://github.com/chischte/insomnia-delay-library
 
 //******************************************************************************
 // DEFINE NAMES AND SEQUENCE OF STEPS FOR THE MAIN CYCLE:
@@ -30,6 +75,21 @@ enum mainCycleSteps {
   endOfMainCycleEnum
 };
 byte numberOfMainCycleSteps = endOfMainCycleEnum;
+
+// DEFINE NAMES TO DISPLAY ON THE TOUCH SCREEN:
+String cycleName[] = { "BremszylinderZurueckfahren", "WippenhebelZiehen1", "BandVorschieben",
+        "BandKlemmen", "BandSpannen", "SpannkraftAufbau", "BandSchneiden", "Schweissen",
+        "WippenhebelZiehen2", "BandklemmeLoesen" };
+
+// SET UP EEPROM COUNTER:
+//******************************************************************************
+// SETUP EEPROM ERROR LOG:
+// create one storage slot for every mainCycleStep to count and
+// store how often there has been a timeout.
+int numberOfEepromValues = (endOfMainCycleEnum - 1);
+int eepromSize = 4096;
+EEPROM_Counter eepromErrorLog(eepromSize, numberOfEepromValues);
+
 //******************************************************************************
 // DECLARATION OF VARIABLES / DATA TYPES
 //******************************************************************************
@@ -37,7 +97,7 @@ byte cycleStep = 0;
 bool autoMode = 0; // Betriebsmodus 0 = Step, 1 = Automatik
 bool stepModeRunning = false;
 bool autoModeRunning = false;
-
+bool machineRunning = false;
 //******************************************************************************
 // GENERATE INSTANCES OF CLASSES:
 //******************************************************************************
@@ -53,7 +113,39 @@ Debounce ModeSwitch(CONTROLLINO_A2);
 Debounce EndSwitchLeft(CONTROLLINO_A5);
 Debounce EndSwitchRight(CONTROLLINO_A0);
 Debounce StrapDetectionSensor(A1);
+
+///////////////////////////////////////////////////////////////
+//TODO INSERT TIMERS HERE
+///////////////////////////////////////////////////////////////
+//Insomnia nextStepTimer;
+//Insomnia errorBlinkTimer;
+//Insomnia resetTimeout;
+Insomnia errorPrintDelay;
 //******************************************************************************
+
+//void TestRigReset() {
+//  ToolReset();
+//  ZylGummihalter.set(0);
+//  ZylFalltuerschieber.set(0);
+//  ZylMagnetarm.set(0);
+//  MotFeedOben.set(0);
+//  MotFeedUnten.set(0);
+//  ZylMesser.set(0);
+//  ZylRevolverschieber.set(0);
+//  machineRunning = false;
+//  errorBlink = false;
+//  stepMode = true;
+//  cycleStep = 0;
+//}
+//void ToolReset() {
+//  // SIMULIERE WIPPENHEBEL ZIEHEN:
+//  digitalWrite(CONTROLLINO_RELAY_08, LOW);  //WIPPENSCHALTER WHITE CABLE (NO)
+//  digitalWrite(CONTROLLINO_RELAY_09, HIGH); //WIPPENSCHALTER RED   CABLE (NC)
+//  delay(200);
+//  // SIMULIERE WIPPENHEBEL LOSLASEN:
+//  digitalWrite(CONTROLLINO_RELAY_09, LOW);  //WIPPENSCHALTER RED   CABLE (NC)
+//  digitalWrite(CONTROLLINO_RELAY_08, HIGH); //WIPPENSCHALTER WHITE CABLE (NO)delay(200);
+//}
 
 void ResetCylinderStates() {
   BremsZylinder.set(0);
@@ -76,6 +168,115 @@ unsigned long ReadCoolingPot() {
   unsigned long coolingTime = map(potVal, 1023, 0, 4000, 14000); // Abkühlzeit min 4, max 14 Sekunden
   return coolingTime;
 }
+
+void PrintErrorLog() {
+
+}
+void RunResetTimeout() {
+
+}
+
+//******************************************************************************
+// MAIN TEST CYCLE:
+//******************************************************************************
+void RunMainTestCycle() {
+  switch (cycleStep) {
+
+  //******************************************************************************
+//SCHALTER DEAKTIVIERT UND ZUR FEHLERSUCHE DURCH TIMER ERSETZT
+//    case BremszylinderZurueckfahren:
+//      if (!EndSwitchLeft.requestButtonState()) { // Bremszylinder ist nicht in Startposition
+//        BremsZylinder.set(1);
+//      } else {
+//        BremsZylinder.set(0);
+//        SwitchToNextStep();
+//      }
+//      break;
+
+  case BremszylinderZurueckfahren:
+    BremsZylinder.stroke(2000, 0);
+    if (BremsZylinder.stroke_completed()) {
+      SwitchToNextStep();
+    }
+    break;
+    //******************************************************************************
+  case WippenhebelZiehen1:
+    WippenhebelZylinder.stroke(1500, 1000);
+    if (WippenhebelZylinder.stroke_completed()) {
+      SwitchToNextStep();
+    }
+    break;
+
+  case BandVorschieben:
+    SpanntastenZylinder.stroke(550, 0);
+    if (SpanntastenZylinder.stroke_completed()) {
+      SwitchToNextStep();
+    }
+    break;
+
+  case BandKlemmen:
+    BandKlemmZylinder.set(1);
+    SwitchToNextStep();
+    break;
+    //******************************************************************************
+//SCHALTER DEAKTIVIERT UND ZUR FEHLERSUCHE DURCH TIMER ERSETZT
+//    case BandSpannen:
+//         SpanntastenZylinder.set(1);
+//         if (EndSwitchRight.requestButtonState()) {
+//           SwitchToNextStep();
+//           stepModeRunning = true; // springe direkt zum nächsten Step
+//         }
+//         break;
+
+  case BandSpannen:
+    SpanntastenZylinder.stroke(3000, 500);
+    if (SpanntastenZylinder.stroke_completed()) {
+      SwitchToNextStep();
+      stepModeRunning = true; // springe direkt zum nächsten Step
+    }
+    break;
+    //******************************************************************************
+
+//    case SpannkraftAufbau:
+//         SpanntastenZylinder.stroke(800, 0); // kurze Pause für Spannkraftaufbau
+//         if (SpanntastenZylinder.stroke_completed()) {
+//           SwitchToNextStep();
+//         }
+//         break;
+
+  case SpannkraftAufbau:
+    SwitchToNextStep();
+    stepModeRunning = true; // springe direkt zum nächsten Step
+    break;
+    //******************************************************************************
+  case BandSchneiden:
+    MesserZylinder.stroke(2000, 2000);
+    if (MesserZylinder.stroke_completed()) {
+      SwitchToNextStep();
+    }
+    break;
+
+  case Schweissen:
+    SchweisstastenZylinder.stroke(500, ReadCoolingPot());
+    if (SchweisstastenZylinder.stroke_completed()) {
+      SwitchToNextStep();
+    }
+    break;
+
+  case WippenhebelZiehen2:
+    WippenhebelZylinder.stroke(1500, 1000);
+    if (WippenhebelZylinder.stroke_completed()) {
+      SwitchToNextStep();
+    }
+    break;
+
+  case BandklemmeLoesen:
+    BandKlemmZylinder.set(0);
+    SwitchToNextStep();
+    break;
+  }
+}
+//******************************************************************************
 
 void setup() {
   ResetCylinderStates();
@@ -102,104 +303,17 @@ void loop() {
     cycleStep = 0;
     ResetCylinderStates();
   }
-  //******************************************************************************
-  // MAIN CYCLE:
-  //******************************************************************************
-  if ((autoMode && autoModeRunning) || (!autoMode && stepModeRunning)) {
-    switch (cycleStep) {
 
-    //******************************************************************************
-//SCHALTER DEAKTIVIERT UND ZUR FEHLERSUCHE DURCH TIMER ERSETZT
-//    case BremszylinderZurueckfahren:
-//      if (!EndSwitchLeft.requestButtonState()) { // Bremszylinder ist nicht in Startposition
-//        BremsZylinder.set(1);
-//      } else {
-//        BremsZylinder.set(0);
-//        SwitchToNextStep();
-//      }
-//      break;
+  // BESTIMMEN OB TEST RIG LÄUFT ODER NICHT
+  machineRunning = ((autoMode && autoModeRunning) || (!autoMode && stepModeRunning));
 
-    case BremszylinderZurueckfahren:
-      BremsZylinder.stroke(2000, 0);
-      if (BremsZylinder.stroke_completed()) {
-        SwitchToNextStep();
-      }
-      break;
-      //******************************************************************************
-    case WippenhebelZiehen1:
-      WippenhebelZylinder.stroke(1500, 1000);
-      if (WippenhebelZylinder.stroke_completed()) {
-        SwitchToNextStep();
-      }
-      break;
+  // AUFRUFEN DER UNTERFUNKTIONEN JE NACHDEM OB DAS RIG LÄUFT ODER NICHT:
+  if (machineRunning) {
+    RunMainTestCycle();
+    RunResetTimeout();
 
-    case BandVorschieben:
-      SpanntastenZylinder.stroke(550, 0);
-      if (SpanntastenZylinder.stroke_completed()) {
-        SwitchToNextStep();
-      }
-      break;
-
-    case BandKlemmen:
-      BandKlemmZylinder.set(1);
-      SwitchToNextStep();
-      break;
-      //******************************************************************************
-//SCHALTER DEAKTIVIERT UND ZUR FEHLERSUCHE DURCH TIMER ERSETZT
-//    case BandSpannen:
-//         SpanntastenZylinder.set(1);
-//         if (EndSwitchRight.requestButtonState()) {
-//           SwitchToNextStep();
-//           stepModeRunning = true; // springe direkt zum nächsten Step
-//         }
-//         break;
-
-    case BandSpannen:
-      SpanntastenZylinder.stroke(3000, 500);
-      if (SpanntastenZylinder.stroke_completed()) {
-        SwitchToNextStep();
-        stepModeRunning = true; // springe direkt zum nächsten Step
-      }
-      break;
-      //******************************************************************************
-
-//    case SpannkraftAufbau:
-//         SpanntastenZylinder.stroke(800, 0); // kurze Pause für Spannkraftaufbau
-//         if (SpanntastenZylinder.stroke_completed()) {
-//           SwitchToNextStep();
-//         }
-//         break;
-
-    case SpannkraftAufbau:
-      SwitchToNextStep();
-      stepModeRunning = true; // springe direkt zum nächsten Step
-      break;
-      //******************************************************************************
-    case BandSchneiden:
-      MesserZylinder.stroke(2000, 2000);
-      if (MesserZylinder.stroke_completed()) {
-        SwitchToNextStep();
-      }
-      break;
-
-    case Schweissen:
-      SchweisstastenZylinder.stroke(500, ReadCoolingPot());
-      if (SchweisstastenZylinder.stroke_completed()) {
-        SwitchToNextStep();
-      }
-      break;
-
-    case WippenhebelZiehen2:
-      WippenhebelZylinder.stroke(1500, 1000);
-      if (WippenhebelZylinder.stroke_completed()) {
-        SwitchToNextStep();
-      }
-      break;
-
-    case BandklemmeLoesen:
-      BandKlemmZylinder.set(0);
-      SwitchToNextStep();
-      break;
-    }
+  } else {
+    PrintErrorLog();
   }
+
 }
